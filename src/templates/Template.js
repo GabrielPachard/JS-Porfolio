dfdcbf8360ec5d4ca483871fe844e774047d7529
Gabriel Pachard <gabrielpachard95@gmail.com>
2022-08-05 14:41:41 -0500

    JS-Porfolio

--- src/templates/Template.js
+++ src/templates/Template.js
@@ -1,4 +1,7 @@
-import getData from '../utils/getData.js';
+import getData from '@utils/getData.js';
+import github from '@images/github.png';
+import twitter from '@images/twitter.png';
+import instagram from '@images/instagram.png';
 
 const Template = async () => {
   const data = await getData();
@@ -7,30 +10,30 @@ const Template = async () => {
       <div class="card">
         <div class="card_details">
           <div class="card_photo center circle">
-            <img src="${data.picture.large}" alt="${data.name.first}">
+            <img src="${ data.picture.large }" alt="${ data.name.first }">
             <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" style="enable-background:new -580 439 577.9 194;"
               xml:space="preserve">
               <circle cx="50" cy="50" r="40" />
             </svg>
           </div>
           <p class="card_title">Hi, My name is</p>
-          <p class="card_value">${data.name.first} ${data.name.last}</p>
+          <p class="card_value">${ data.name.first } ${ data.name.last }</p>
         </div>
         <div class="card_userdata">
           <ul>
-            <li>${data.email}</li>
-            <li>${data.location.country}</li>
+            <li>${ data.email }</li>
+            <li>${ data.location.country }</li>
           </ul>
         </div>
         <div class="card_social">
           <a href="https://twitter.com/gndx">
-            <img src="../src/assets/images/twitter.png" />
+            <img src="${ twitter }" />
           </a>
           <a href="https://github.com/gndx">
-            <img src="../src/assets/images/github.png" />
+            <img src="${ github }" />
           </a>
           <a href="https://instagram.com/gndx">
-            <img src="../src/assets/images/instagram.png" />
+            <img src="${ instagram }" />
           </a>
         </div>
       </div>
@@ -39,4 +42,4 @@ const Template = async () => {
   return view;
 };
 
-export default Template;
+export  { Template };
